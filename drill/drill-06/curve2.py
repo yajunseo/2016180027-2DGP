d624574fef31167989f54295a360b0645f1033bb
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2019-10-11 14:29:06 +0900

    drill-06-2 진행중
    진행중

--- drill/drill-06/curve2.py
+++ drill/drill-06/curve2.py
@@ -35,17 +35,24 @@ def draw_curve_3_points(p1, p2, p3, p4):
     t = 0
     global frame
     global running
-    while t < 998 / 1000:
-        for i in range(0, 1000, 2):
-            t = i / 1000
-            x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+    while t < 9998 / 10000:
+        for i in range(0, 10000 + 1, 2):
+
+
+            t = i / 10000
+            cx = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
-            y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+            cy = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
                     -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
             clear_canvas()
             kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
+            if x < cx:
+                character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
+            elif x >= cx:
+                character.clip_draw(frame * 100, 0 * 1, 100, 100, cx, cy)
             update_canvas()
+            x, y = cx, cy
             frame = (frame + 1) % 8
 
 
@@ -56,8 +63,9 @@ r = 0
 while running:
     update_canvas()
     while True:
-        draw_curve_3_points(p[r], p[(r+1) % 10], p[(r+2) % 10], p[(r+3) % 10])
+        draw_curve_3_points(p[r % 10], p[(r+1) % 10], p[(r+2) % 10], p[(r+3) % 10])
         r += 1
+
 
     handle_events()
 
