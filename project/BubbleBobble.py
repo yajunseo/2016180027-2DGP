17dff85323b11e110c445f773084c6e97b35a1fe
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2019-10-15 17:16:58 +0900

    프로젝트 작업(10.15)
    캐릭터 객체화

--- project/BubbleBobble.py
+++ project/BubbleBobble.py
@@ -9,54 +9,112 @@ move_left = load_image('move.png')
 move_right = load_image('move-r.png')
 x = 300
 y = 0
-jump_x = 0
-jump_y = 0
+temp_x = 0
+temp_y = 0
 frame = 0
 running = True
-side = 1
+look_side = 1
 dir = 0
-
-def draw_curve_3_points(p1, p2, p3):
-    global cx, cy
-    global x, y
-    t = 0
-    global frame
-    global running
-    while t < 998 / 1000:
-        for i in range(0, 1000 + 1, 2):
-            t = i / 1000
+is_jump = 0
+block_size = 70
 
 
-            cx = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
-            cy = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+class Hero:
+    def __init__(self):
+        self.x, self.y = 300, 30
+        self.jump_x, self.jump_y = self.x, self.y
+        self.frame = 0
 
-            clear_canvas()
-            stage1.draw(map_width // 2, map_height // 2)
-
-            if x < cx:
-                move_right.clip_draw(frame * 47, 10, 40, 40, x, y)
-            elif x >= cx:
-                move_left.clip_draw(frame * 47 , 55, 40, 40, x, y)
-            update_canvas()
-
-            y = cy
-            if cx > 70 and cx < 900:
-                x = cx
-
+    def update(self):
+        global is_jump
+        global dir
+        if is_jump == 1:
+            self.jump_y = self.y + 200
+            if dir == 0:
+                if look_side == 1:
+                    self.jump_x = self.x + 5
+                elif look_side == -1:
+                    self.jump_x = self.x - 5
             else:
-                if x == 70:
-                    x == 80
-                if x >= 900:
-                    x == 890
-            frame = (frame + 1) % 7
+                if look_side == 1:
+                    self.jump_x = self.x + 200
+                elif look_side == -1:
+                    self.jump_x = self.x - 200
+
+        self.frame = (self.frame + 1) % 7
+
+    def draw(self):
+        global is_jump
+        global block_size
+        global map_width
+        global dir
+        global look_side
+
+        if is_jump == 1:
+            p1, p2, p3 = (self.x, self.y), ((self.jump_x + self.x) / 2, self.jump_y), (self.jump_x, self.y)
+            t = 0
+            p1_x, p1_y = p1[0], p1[1]
+            while t < 998 / 1000:
+                for i in range(0, 1000 + 1, 2):
+                    t = i / 1000
+                    temp_x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+                    temp_y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+
+                    clear_canvas()
+                    stage1.draw(map_width // 2, map_height // 2)
+
+                    if p1_x < temp_x:
+                        move_right.clip_draw(self.frame * 47, 10, 40, 40, p1_x, p1_y)
+                    elif p1_x >= temp_x:
+                        move_left.clip_draw(self.frame * 47, 55, 40, 40, p1_x, p1_y)
+                    update_canvas()
+
+
+                    p1_y = temp_y
+
+                    if (temp_x > block_size) and (temp_x < (map_width - block_size)):
+                        p1_x = temp_x
+
+                    else:
+                        if p1_x <= block_size:
+                            p1_x = (block_size + 10)
+                        if p1_x >= (map_width - block_size):
+                            p1_x = (map_width - block_size - 10)
+                    self.frame = (self.frame + 1) % 7
+            is_jump = 0
+            self.x = p1_x
+            self.y = p1_y
+
+        else:
+            if (self.x >= (block_size)) and self.x <= (map_width - block_size):
+                if dir < 0:
+                    move_left.clip_draw(self.frame * 47, 55, 40, 40, self.x, self.y)
+                elif dir > 0:
+                    move_right.clip_draw(self.frame * 47, 10, 40, 40,self.x, self.y)
+                else:
+                    if look_side == 1:
+                        move_right.clip_draw(self.frame * 47, 10, 40, 40, self.x, self.y)
+                    elif look_side == -1:
+                        move_left.clip_draw(self.frame * 47, 55, 40, 40, self.x, self.y)
+                self.x += dir * 2
+                if self.x <= block_size:
+                    self.x += 10
+                if self.x >= map_width - block_size:
+                    self.x -= 10
+            else:
+                if look_side == 1:
+                    move_right.clip_draw(self.frame * 47, 10, 40, 40, self.x, self.y)
+                elif look_side == -1:
+                    move_left.clip_draw(self.frame * 47, 55, 40, 40, self.x, self.y)
+
 
 
 def handle_events():
     global running
     global x, y
-    global jump_x, jump_y
     global dir
-    global side
+    global look_side
+    global is_jump
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -65,17 +123,12 @@ def handle_events():
             run = 1
             if event.key == SDLK_LEFT:
                 dir -= 1
-                side = -1
+                look_side = -1
             if event.key == SDLK_RIGHT:
                 dir += 1
-                side = 1
+                look_side = 1
             if event.key == SDLK_SPACE:
-                jump_y = y + 200
-                if side == 1:
-                    jump_x = x + 200
-                else:
-                    jump_x = x - 200
-                draw_curve_3_points((x, y), ((jump_x + x)/2, jump_y), (jump_x, y))
+                is_jump = 1
 
         if event.type == SDL_KEYUP:
             run = 0
@@ -86,34 +139,15 @@ def handle_events():
 
 
 
+main_hero = Hero()
 
 
 while running:
+
+    handle_events()
+    update_canvas()
+    main_hero.update()
     clear_canvas()
     stage1.draw(map_width//2, map_height//2)
+    main_hero.draw()
 
-    if x >= 70 and x <= 900:
-        if dir < 0:
-            move_left.clip_draw(frame * 47 , 55, 40, 40, x, 30)
-        elif dir > 0:
-            move_right.clip_draw(frame * 47, 10, 40, 40, x, 30)
-        else:
-            if side == 1:
-                move_right.clip_draw(frame * 47, 10, 40, 40, x, 30)
-            elif side == -1:
-                move_left.clip_draw(frame * 47, 55, 40, 40, x, 30)
-        x += dir * 2
-        if x <= 70:
-            x += 10
-        if x >= 900:
-            x -= 10
-    else:
-        if side == 1:
-            move_right.clip_draw(frame * 47, 10, 40, 40, x, 30)
-        elif side == -1:
-            move_left.clip_draw(frame * 47, 55, 40, 40, x, 30)
-
-
-    update_canvas()
-    frame = (frame + 1) % 7
-    handle_events()
