3929ad8e9520f754c09763920b0eb42f254d260f
yajunseo  - C105 <55369207+yajunseo@users.noreply.github.com>
2019-12-02 12:42:51 +0900

    진행중

--- drill/drill-14/ball.py
+++ drill/drill-14/ball.py
@@ -9,7 +9,7 @@ class Ball:
     def __init__(self):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y = random.randint(0, 1600-1), random.randint(0, 1200-1)
+        self.x, self.y = random.randint(0, 1200-1), random.randint(0, 1200-1)
         self.canvas_width = get_canvas_width()
         self.canvas_height = get_canvas_height()
         self.bg = 0
@@ -20,7 +20,6 @@ class Ball:
 
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
-        return 0,0,0,0
 
     def draw(self):
         self.image.draw(self.x - self.bg.window_left, self.y - self.bg.window_bottom)
